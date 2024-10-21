11b878d0440d8d105299f86957a50ac55de10e02
Vishal Singh <vishal.singh2114@gmail.com>
2024-10-21 23:05:05 +0530

    Updating Files

--- 5.0- Searching and Sorting Algorithm/3.+Bubble+Sort.py
+++ 5.0- Searching and Sorting Algorithm/3.+Bubble+Sort.py
@@ -1,14 +1,14 @@
 def bubble_sort(arr):
     n = len(arr)
 
-    for i in range(0,n-1):
-        for j in range(n-1-i):
+    for passes in range(0,n):
+        for j in range(0,n-1-passes):
             if(arr[j] > arr[j+1]):
                 arr[j],arr[j+1] = arr[j+1],arr[j]
-                
+
     return arr
 
 
-unsorted_list = [1,10,64,34,25,12,22]
+unsorted_list = [120,25,11,34,90,22]
 sorted_list = bubble_sort(unsorted_list)
-print(sorted_list)
+print("Sorted Elements :", sorted_list)
